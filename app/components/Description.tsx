83ebdf16d695de9ea3bdf29dcd514b2df139ab84
Gizem Gök <gizemgk654@gmail.com>
2023-07-03 15:20:48 +0300

    eslint

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Description() {
     return (
-        <div >
+        <div>
             <p className='text-7xl font-semibold text-white'>Hacer Gizem Gök</p>
             <p className='mt-4 mb-10 text-base font-normal text-gray-300'>I graduated from the Computer Engineering department. I've been interested in Frontend Development for almost two year. Every day, I dedicate myself to learning new technologies and continuously improving my skills.</p>
         </div>
