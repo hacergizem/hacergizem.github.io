bc3e84bf962cf52fc211c6bc7e56104d10a9b897
Gizem Gök <gizemgk654@gmail.com>
2023-07-03 15:46:56 +0300

    deployment problem

--- app/components/Description.tsx
+++ app/components/Description.tsx
@@ -4,7 +4,7 @@ function Description() {
     return (
         <div>
             <p className='text-7xl font-semibold text-white'>Hacer Gizem Gök</p>
-            <p className='mt-4 mb-10 text-base font-normal text-gray-300'>I graduated from the Computer Engineering department. I've been interested in Frontend Development for almost two year. Every day, I dedicate myself to learning new technologies and continuously improving my skills.</p>
+            <p className='mt-4 mb-10 text-base font-normal text-gray-300'>I graduated from the Computer Engineering department. I have been interested in Frontend Development for almost two year. Every day, I dedicate myself to learning new technologies and continuously improving my skills.</p>
         </div>
     )
 }
